1ea9eba0048011268fa8c4b9b7ca9d97148266e9
Duha Mamdouh <eslam.mamdoh15@gmail.com>
2019-04-12 21:21:14 +0200

    create login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './layout/header/header/header.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { LoginComponent } from './components/login/login.component';
+import { LoginFormComponent } from './components/login/login-form/login-form.component';
+import { NavbarComponent } from './layout/header/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    LoginComponent,
+    LoginFormComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule
